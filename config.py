23b2fb3272f31343cd0ea6df25a028d44bd57c43
issachar5 <193503195+issachar5@users.noreply.github.com>
2025-04-22 10:10:27 +0300

    config file, ssh & telegram

---
+++ config.py
@@ -0,0 +1,6 @@
+TELEGRAM_TOKEN = "YOUR_BOT_TOKEN"
+AUTHORIZED_USER_IDS = [123456789]  # Your Telegram user ID
+SSH_HOST = "your.scanning.server"
+SSH_PORT = 22
+SSH_USERNAME = "your_username"
+SSH_KEY_PATH = "/path/to/ssh_key"  # Or use password auth
